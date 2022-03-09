3848e6dbb3be5571dc24c5e07c4cd75b88f2ae77
Brendo Sousa <brendo.n.s.sousa@gmail.com>
2022-03-09 08:49:05 -0300

    build: database for products

---
+++ src/db/db.js
@@ -0,0 +1,43 @@
+const db = [
+  {
+    name: "Dom Casmurro",
+    author: "Machado de Assis",
+    price: 35,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_855423-MLB41572489335_042020-O.jpg",
+  },
+  {
+    name: "Terra Papagalli",
+    author: "J. R. Torero & M. A. Pimenta",
+    price: 35,
+    image:
+      "https://albericovirtual.albericorodrigues.com.br/wp-content/uploads/2020/09/terra-papagalli.jpg",
+  },
+  {
+    name: "O Caso dos Dez Negrinhos",
+    author: "Aghata Christie",
+    price: 39.99,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_855423-MLB41572489335_042020-O.jpg",
+  },
+  {
+    name: "A Revolução dos Bichos",
+    author: "George Orwell",
+    price: 40,
+    image: "https://images-na.ssl-images-amazon.com/images/I/91BsZhxCRjL.jpg",
+  },
+  {
+    name: "Vidas Secas",
+    author: "Graciliano Ramos",
+    price: 45.5,
+    image: "https://images-na.ssl-images-amazon.com/images/I/71iF-Hv07yL.jpg",
+  },
+  {
+    name: "A Hora da Estrela",
+    author: "Clarice Lispector",
+    price: 29.99,
+    image: "https://images-na.ssl-images-amazon.com/images/I/810Vj9zyi-L.jpg",
+  },
+];
+
+export default db;
